c440867b5181edb67850c0c42288f26141d10ebd
Chad Weimer <git@cwmr.dev>
2023-02-02 22:05:59 +0000

    Added some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Make ls better
+alias ls='ls --color -h --group-directories-first'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
