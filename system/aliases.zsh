4338d15ff01267e9ca59975a06b5c3b5d4802bd6
Chad Weimer <git@cwmr.dev>
2023-02-02 22:13:17 -0800

    Fixed ls aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,5 +1,5 @@
 # Make ls better
-alias ls='ls --color -h --group-directories-first'
-alias ll='ls -l'
-alias la='ls -a'
-alias lla='ls -la'
+alias ls='ls -h --color --group-directories-first'
+alias ll='ls -lh --color --group-directories-first'
+alias la='ls -a --color --group-directories-first'
+alias lla='ls -lah --color --group-directories-first'
