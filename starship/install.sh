b30cd9a059ed30246c95b6a8605e920fdf53cda9
Chad Weimer <git@cwmr.dev>
2023-02-02 20:03:47 -0800

    Change starship install dir

--- starship/install.sh
+++ starship/install.sh
@@ -2,7 +2,7 @@
 
 # Check for starship
 if test ! $(which starship); then
-    curl -sS https://starship.rs/install.sh | sh -s -- -y
+    curl -sS https://starship.rs/install.sh | sh -s -- -y -b $HOME/bin
 fi
 
 exit 0
