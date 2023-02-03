287f1adb094ee7f6f91a84c50e6cebd8ec027072
Chad Weimer <git@cwmr.dev>
2023-02-02 20:26:12 -0800

    Don't require root priv

--- starship/install.sh
+++ starship/install.sh
@@ -2,7 +2,7 @@
 
 # Check for starship
 if test ! $(which starship); then
-    curl -sS https://starship.rs/install.sh | sh -s -- -y -b $HOME/bin
+    curl -sS https://starship.rs/install.sh | sh -s -- -y -b $ZSH/bin
 fi
 
 exit 0
