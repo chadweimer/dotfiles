9f529d519c57e67865a446df437bc570f5e0c0d1
Chad Weimer <git@cwmr.dev>
2023-02-03 00:51:20 +0000

    Use xclip to emulate pbcopy

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,3 +3,5 @@ alias ls='ls --color -h --group-directories-first'
 alias ll='ls -l'
 alias la='ls -a'
 alias lla='ls -la'
+
+alias pbcopy='xclip -selection clipboard'
---
+++ system/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Usage: pkginst <pkg>...
+# Description: install a packge via the distro's package manager
+pkginst () {
+    if test $(which pacman); then
+        pacman -yS $*
+    elif test $(which apt-get); then
+        apt-get install -y $*
+    elif test $(which dnf); then
+        dnf install -y $*
+    elif test $(which yum); then
+        yum install -y $*
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+# Install xclip
+if ! test $(which xclip); then
+    pkginst xclip
+fi
