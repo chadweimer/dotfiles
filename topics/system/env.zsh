22b5ad10e1de87d4e8f041107d51dc069c1b763a
Chad Weimer <git@cwmr.dev>
2023-07-17 20:40:08 -0700

    Use gnome keyring

--- topics/system/env.zsh
+++ topics/system/env.zsh
@@ -1,2 +1,7 @@
 export EDITOR='nano'
 export LESS='-R --use-color -Dd+r$Du+b'
+
+# Use GNOME Keyring as ssh agent, if available
+if [[ ! -v SSH_AUTH_SOCK && -e $XDG_RUNTIME_DIR/gcr/ssh ]]; then
+  export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
+fi
