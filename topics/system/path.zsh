086240023bbc324b1e9c7a51b75d9d7c9479e69e
Chad Weimer <git@cwmr.dev>
2024-07-07 12:49:05 -0700

    Add user's bin folder to PATH

--- topics/system/path.zsh
+++ topics/system/path.zsh
@@ -1,1 +1,9 @@
 export -U PATH="$ZSH/bin:$PATH:/usr/sbin"
+
+# Add user's bin folders, if they exist
+if [ -d "$HOME/.local/bin" ]; then
+  export -U PATH="$HOME/.local/bin:$PATH"
+fi
+if [ -d "$HOME/bin" ]; then
+  export -U PATH="$HOME/bin:$PATH"
+fi
