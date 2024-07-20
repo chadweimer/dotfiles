ba2a82648cffd591d24e810d3045072a2d8a6390
Chad Weimer <git@cwmr.dev>
2024-07-20 00:25:35 -0700

    Fix typo

--- topics/xdg/path.zsh
+++ topics/xdg/path.zsh
@@ -1,3 +1,3 @@
 export ANSIBLE_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/ansible
 export CUDA_CACHE_PATH="${XDG_CACHE_HOME:-$HOME/.cache}"/nv
-export DOTNET_CLI_HOME="${$XDG_DATA_HOME:-$HOME/.local/share}"/dotnet
+export DOTNET_CLI_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/dotnet
