5b89ef6c57077bbedaf3e7caeccd861d7b73d887
Chad Weimer <git@cwmr.dev>
2024-08-04 12:20:11 -0700

    Fall back to ssh-agent

--- topics/system/env.zsh
+++ topics/system/env.zsh
@@ -1,7 +1,17 @@
 export EDITOR='nano'
 export LESS='-R --use-color -Dd+r$Du+b'
 
-# Use GNOME Keyring as ssh agent, if available
+# Prefer GNOME Keyring as ssh agent, if available
 if [[ ! -v SSH_AUTH_SOCK && -e $XDG_RUNTIME_DIR/gcr/ssh ]]; then
   export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
+elif (( $+commands[ssh-agent] )); then
+  # Start the ssh agent, if not already running
+  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    eval $(ssh-agent -s) > /dev/null
+    # if [ "$(ssh-add -l)" = "The agent has no identities." ] ; then
+    #     # Auto-add ssh keys to your ssh agent
+    #     # Example:
+    #     # ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+    # fi
+  fi
 fi
