24333b3a191ab239e7969397fceeffa3a9f78ffa
Chad Weimer <git@cwmr.dev>
2024-08-04 13:53:39 -0700

    Use ksshaskpass when present

--- topics/system/env.zsh
+++ topics/system/env.zsh
@@ -5,6 +5,12 @@ export LESS='-R --use-color -Dd+r$Du+b'
 if [[ ! -v SSH_AUTH_SOCK && -e $XDG_RUNTIME_DIR/gcr/ssh ]]; then
   export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
 elif (( $+commands[ssh-agent] )); then
+  # Use KDE Wallet, if available
+  if (( $+commands[ksshaskpass] )); then
+    export SSH_ASKPASS=$(which ksshaskpass)
+    export SSH_ASKPASS_REQUIRE=prefer
+  fi
+
   # Start the ssh agent, if not already running
   if ! pgrep -u "$USER" ssh-agent > /dev/null; then
     eval $(ssh-agent -s) > /dev/null
