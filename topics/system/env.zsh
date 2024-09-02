0597d005cd3c9eff3c564c5401da888e35892fe3
Chad Weimer <git@cwmr.dev>
2024-09-02 10:49:06 -0700

    Only config ssh-agent when $XDG_RUNTIME_DIR is set

--- topics/system/env.zsh
+++ topics/system/env.zsh
@@ -8,21 +8,23 @@ elif (( $+commands[nano] )); then
   export EDITOR='nano'
 fi
 
-# Prefer GNOME Keyring as ssh agent, if available
-if [[ ! -v SSH_AUTH_SOCK && -e $XDG_RUNTIME_DIR/gcr/ssh ]]; then
-  export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
-elif (( $+commands[ssh-agent] )); then
-  # Use KDE Wallet, if available
-  if (( $+commands[ksshaskpass] )); then
-    export SSH_ASKPASS=$(which ksshaskpass)
-    export SSH_ASKPASS_REQUIRE=prefer
-  fi
+if [[ -v XDG_RUNTIME_DIR ]]; then
+  # Prefer GNOME Keyring as ssh agent, if available
+  if [[ ! -v SSH_AUTH_SOCK && -e $XDG_RUNTIME_DIR/gcr/ssh ]]; then
+    export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
+  elif (( $+commands[ssh-agent] )); then
+    # Use KDE Wallet, if available
+    if (( $+commands[ksshaskpass] )); then
+      export SSH_ASKPASS=$(which ksshaskpass)
+      export SSH_ASKPASS_REQUIRE=prefer
+    fi
 
-  # Start the ssh agent, if not already running
-  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -s > "$XDG_RUNTIME_DIR/ssh-agent.env"
-  fi
-  if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+    # Start the ssh agent, if not already running
+    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+      ssh-agent -s > "$XDG_RUNTIME_DIR/ssh-agent.env"
+    fi
+    if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+      source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+    fi
   fi
 fi
