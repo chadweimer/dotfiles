b283d7629661fa14f4ae7aa81ddda3149c47284f
Chad Weimer <git@cwmr.dev>
2024-09-07 15:11:09 -0700

    Don't skip install when already installed

--- topics/docker/install.sh
+++ topics/docker/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Check for devpod
-if [ ! -f "$ZSH/bin/devpod" ]; then
-    curl -L -o "$ZSH/bin/devpod" https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 && chmod 0755 "$ZSH/bin/devpod"
-fi
+echo "› Installing devpod..."
+curl -L -o "$ZSH/bin/devpod" https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 && chmod 0755 "$ZSH/bin/devpod"
+echo "› Installed devpod"
+echo ""
 
 exit 0
--- topics/starship/install.sh
+++ topics/starship/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Check for starship
-if [ ! -f "$ZSH/bin/starship" ]; then
-    curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$ZSH/bin"
-fi
+echo "› Installing starship..."
+curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$ZSH/bin"
+echo "› Installed starship"
+echo ""
 
 exit 0
