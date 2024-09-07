ac072ee8a7e70adbb7ba1ddc8ba8e6464d281659
Chad Weimer <git@cwmr.dev>
2024-09-07 14:56:42 -0700

    linting

--- topics/starship/install.sh
+++ topics/starship/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Check for starship
-if [ ! -f $ZSH/bin/starship ]; then
-    curl -sS https://starship.rs/install.sh | sh -s -- -y -b $ZSH/bin
+if [ ! -f "$ZSH/bin/starship" ]; then
+    curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$ZSH/bin"
 fi
 
 exit 0
