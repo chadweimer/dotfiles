1263391ac5d067c033c31a70cff5e6e810181b88
Chad Weimer <git@cwmr.dev>
2025-01-02 08:12:30 -0800

    Check if tmux is installed first

--- topics/system/tmux.zsh
+++ topics/system/tmux.zsh
@@ -1,3 +1,3 @@
-if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]] && (( $+commands[tmux] )); then
   exec tmux new-session -A -s ssh
 fi
