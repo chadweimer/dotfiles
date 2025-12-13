5df6fad3f664e89ca826324de4275914d7c4c0a8
Chad Weimer <git@cwmr.dev>
2025-12-13 14:57:17 -0800

    Adjustments to better support TMux; more to still change

--- topics/docker/install.sh
+++ topics/docker/install.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+case "$(uname -m)" in
+  x86_64)         ARCH="amd64" ;; # 64‑bit Intel/AMD
+  aarch64|armv8*) ARCH="arm64" ;; # 64‑bit ARM (Apple Silicon, AWS Graviton, etc.)
+  *)
+    echo "❌ Unsupported architecture for devpod: $(uname -m)"
+    exit 0
+    ;;
+esac
+
 echo "› Installing devpod..."
-curl -L -o "$ZSH/bin/devpod" https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 && chmod 0755 "$ZSH/bin/devpod"
+curl -L -o "$ZSH/bin/devpod" "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-$ARCH" && chmod 0755 "$ZSH/bin/devpod"
 echo "› Installed devpod"
 echo ""
 
--- topics/starship/install.sh
+++ topics/starship/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Skip installation if starship is already installed and not located at $ZSH/bin/starship
+if command -v starship >/dev/null 2>&1 && [ "$(command -v starship)" != "$ZSH/bin/starship" ]; then
+  echo "› starship is already installed, skipping installation"
+  exit 0
+fi
+
 echo "› Installing starship..."
 curl -sS https://starship.rs/install.sh | sh -s -- -y -b "$ZSH/bin"
 echo "› Installed starship"
