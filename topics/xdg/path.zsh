d3fb44ee76122cd8c02f78a610ee99f61e56e7c2
Chad Weimer <git@cwmr.dev>
2026-01-31 00:12:55 -0800

    More XDG Base Directory

--- topics/xdg/path.zsh
+++ topics/xdg/path.zsh
@@ -1,3 +1,8 @@
-export ANSIBLE_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/ansible
-export CUDA_CACHE_PATH="${XDG_CACHE_HOME:-$HOME/.cache}"/nv
-export DOTNET_CLI_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/dotnet
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+
+export ANSIBLE_HOME="${XDG_DATA_HOME}/ansible"
+export CUDA_CACHE_PATH="${XDG_CACHE_HOME}"/nv
+export DOTNET_CLI_HOME="${XDG_DATA_HOME}"/dotnet
